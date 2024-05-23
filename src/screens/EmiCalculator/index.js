791b062a2afd5a750bcfbc67609ad00a37144103
Adnan Bohra TPR <adnan.bohra.tpr@pwc.com>
2024-05-23 13:35:29 +0530

    EMI calcultor done

--- src/screens/EmiCalculator/index.js
+++ src/screens/EmiCalculator/index.js
@@ -8,7 +8,7 @@ import { TextInput } from 'react-native-paper'
 import Button from '../../components/Button'
 
 
-const EmiCalculator = () => {
+const EmiCalculator = ({ navigation }) => {
     const [sliderValue, setSliderValue] = useState(0);
     const [text, setText] = useState('');
     const [sliderValue2, setSliderValue2] = useState(0);
@@ -20,64 +20,92 @@ const EmiCalculator = () => {
 
     const onSliderValueChange = (value) => {
         setSliderValue(value);
-        setText(value.toString()); // Convert slider value to string and set it in the text input
+        setText(value.toString());
     };
 
     const onSliderValueChange2 = (value) => {
         setSliderValue2(value);
-        setText2(value.toString()); // Convert slider value to string and set it in the text input
+        setText2(value.toString());
     };
 
     const onSliderValueChange3 = (value) => {
         setSliderValue3(value);
-        setText3(value.toString()); // Convert slider value to string and set it in the text input
+        setText3(value.toString());
     };
 
     const onTextInputChange = (inputText) => {
-        setText(inputText);
+        const numericValue = inputText.replace(/[^0-9]/g, '');
+        const numberValue = parseInt(numericValue, 10);
+        if (numberValue > 5000000) {
+            setText('5000000');
+        } else {
+            setText(numericValue.toString());
+        }
     };
+
 
     const onTextInputChange2 = (inputText) => {
-        setText2(inputText);
+        const numericValue = inputText.replace(/[^0-9]/g, '');
+        const numberValue = parseInt(numericValue, 10);
+        if (numberValue > 360) {
+            setText2('360');
+        } else {
+            setText2(numericValue.toString());
+        }
     };
+
 
     const onTextInputChange3 = (inputText) => {
-        setText3(inputText);
+        const numericValue = inputText.replace(/[^0-9]/g, '');
+        const numberValue = parseInt(numericValue, 10);
+        if (numberValue > 25) {
+            setText3('25');
+        } else {
+            setText3(numericValue.toString());
+        }
     };
 
+
     const onTextInputEndEditing = (inputText) => {
-        const numericValue = parseFloat(inputText); // Convert text input value to a number
+        const numericValue = parseFloat(inputText);
         if (!isNaN(numericValue)) {
-            setSliderValue(numericValue); // Set slider value to the numeric value of the text input
+            setSliderValue(numericValue);
         }
     };
 
     const onTextInputEndEditing2 = (inputText) => {
-        const numericValue = parseFloat(inputText); // Convert text input value to a number
+        const numericValue = parseFloat(inputText);
         if (!isNaN(numericValue)) {
-            setSliderValue2(numericValue); // Set slider value to the numeric value of the text input
+            setSliderValue2(numericValue);
         }
     };
 
     const onTextInputEndEditing3 = (inputText) => {
-        const numericValue = parseFloat(inputText); // Convert text input value to a number
+        const numericValue = parseFloat(inputText);
         if (!isNaN(numericValue)) {
-            setSliderValue3(numericValue); // Set slider value to the numeric value of the text input
+            setSliderValue3(numericValue);
         }
     };
 
     const calculateEMI = () => {
+        if (!text || !text2 || !text3) {
+            alert("Please select all the fields.");
+            return;
+        }
         const principal = parseFloat(text);
         const tenureMonths = parseFloat(text2);
-        const interestRate = parseFloat(text3) / 12 / 100; // Convert annual interest rate to monthly and percentage to decimal
-    
-        // Calculate EMI using the formula
+        const interestRate = parseFloat(text3) / 12 / 100;
+        if (principal === 0 || tenureMonths === 0 || interestRate === 0) {
+            alert("Please enter valid values for all the fields.");
+            return;
+        }
         const emi =
-          (principal * interestRate * Math.pow(1 + interestRate, tenureMonths)) /
-          (Math.pow(1 + interestRate, tenureMonths) - 1);
-    
-        setEmi(emi.toFixed(2)); // Update state with calculated EMI
-      };
+            (principal * interestRate * Math.pow(1 + interestRate, tenureMonths)) /
+            (Math.pow(1 + interestRate, tenureMonths) - 1);
+
+        setEmi(emi.toFixed(2));
+    };
+
 
 
     return (
@@ -94,10 +122,11 @@ const EmiCalculator = () => {
                         leftStyle={{ width: 30, height: 30 }} title="EMI Calculator"
                         onPressRight={() => alert("Faq")} />
                 </View>
+                {/* Requested Loan Amount */}
                 <View style={{ justifyContent: 'center', marginTop: verticalScale(30) }}>
                     <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ marginLeft: horizontalScale(20), color: "#C23934", bottom: verticalScale(2) }}>*</Text>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>Requested Loan Amount(₹) </Text>
+                        <Text style={styles.star}>*</Text>
+                        <Text style={styles.title}>Requested Loan Amount(₹) </Text>
                     </View>
                     <View style={{
                         marginVertical: verticalScale(10),
@@ -105,8 +134,7 @@ const EmiCalculator = () => {
                         <Slider
                             value={sliderValue}
                             step={1}
-                            style={{ width: '50%', alignSelf: 'center', transform: [{ scaleX: verticalScale(2), }, { scaleY: verticalScale(2) }], borderRadius: 20 }}
-                            minimumValue={0}
+                            style={styles.sliderstyle} minimumValue={0}
                             maximumValue={5000000}
                             minimumTrackTintColor={"#0070D2"}
                             maximumTrackTintColor="#C4C4C4"
@@ -114,29 +142,30 @@ const EmiCalculator = () => {
                             onValueChange={onSliderValueChange}
                         />
                     </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: horizontalScale(15), marginTop: verticalScale(-8) }}>
-                        <Text style={{ color: "#3E3E3C", fontSize: 16, fontWeight: '400', lineHeight: 18 }}>
+                    <View style={styles.numberView}>
+                        <Text style={styles.number}>
                             ₹ 1L
                         </Text>
-                        <Text style={{ color: "#3E3E3C", fontSize: 16, fontWeight: '400', lineHeight: 18 }}>
-                            ₹ 5Cr
+                        <Text style={styles.number}>
+                            ₹ 50L
                         </Text>
                     </View>
 
                     <TextInput
+                        maxLength={7}
                         keyboardType="numeric"
-                        style={{ borderBottomWidth: 0.5, borderBottomColor: 'grey', backgroundColor: 'transparent', maxWidth: '90%', marginHorizontal: horizontalScale(18) }}
-                        onChangeText={onTextInputChange}
+                        style={styles.inputbox} onChangeText={onTextInputChange}
                         onEndEditing={(event) => onTextInputEndEditing(event.nativeEvent.text)}
                         value={text}
                     />
 
                 </View>
 
-                <View style={{ justifyContent: 'center', marginTop: 28}}>
+                {/* Requested Monthly Tenure */}
+                <View style={{ justifyContent: 'center', marginTop: verticalScale(28) }}>
                     <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ marginLeft: horizontalScale(20), color: "#C23934", bottom: verticalScale(2) }}>*</Text>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>Requested Monthly Tenure</Text>
+                        <Text style={styles.star}>*</Text>
+                        <Text style={styles.title}>Requested Monthly Tenure</Text>
                     </View>
                     <View style={{
                         marginVertical: verticalScale(10),
@@ -144,39 +173,37 @@ const EmiCalculator = () => {
                         <Slider
                             value={sliderValue2}
                             step={1}
-                            style={{ width: '50%', alignSelf: 'center', transform: [{ scaleX: verticalScale(2), }, { scaleY: verticalScale(2) }], borderRadius: 20 }}
-                            minimumValue={0}
+                            style={styles.sliderstyle} minimumValue={0}
                             maximumValue={360}
                             minimumTrackTintColor={"#0070D2"}
-                            maximumTrackTintColor="#C4C4C4"                            thumbTintColor={colors.coreBlue}
+                            maximumTrackTintColor="#C4C4C4" thumbTintColor={colors.coreBlue}
                             onValueChange={onSliderValueChange2}
                         />
                     </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: horizontalScale(15), marginTop: verticalScale(-8) }}>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>
+                    <View style={styles.numberView}>
+                        <Text style={styles.number}>
                             12 Months
                         </Text>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>
+                        <Text style={styles.number}>
                             360 Months
                         </Text>
                     </View>
 
                     <TextInput
+                        maxLength={3}
                         keyboardType="numeric"
-                        style={{ borderBottomWidth: 0.5, borderBottomColor: 'grey', backgroundColor: 'transparent', maxWidth: '90%', marginHorizontal: horizontalScale(18) }}
-                        onChangeText={onTextInputChange2}
+                        style={styles.inputbox} onChangeText={onTextInputChange2}
                         onEndEditing={(event) => onTextInputEndEditing2(event.nativeEvent.text)}
                         value={text2}
-
-
                     />
 
                 </View>
 
-                <View style={{ justifyContent: 'center', marginTop: 28}}>
+                {/* Rate of Interest */}
+                <View style={{ justifyContent: 'center', marginTop: verticalScale(28) }}>
                     <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ marginLeft: horizontalScale(20), color: "#C23934", bottom: verticalScale(2) }}>*</Text>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>Rate Of Interest</Text>
+                        <Text style={styles.star}>*</Text>
+                        <Text style={styles.title}>Rate Of Interest</Text>
                     </View>
                     <View style={{
                         marginVertical: verticalScale(10),
@@ -184,56 +211,50 @@ const EmiCalculator = () => {
                         <Slider
                             value={sliderValue3}
                             step={1}
-                            style={{ width: '50%', alignSelf: 'center', transform: [{ scaleX: verticalScale(2), }, { scaleY: verticalScale(2) }], borderRadius: 20 }}
+                            style={styles.sliderstyle}
                             minimumValue={0}
                             maximumValue={25}
                             minimumTrackTintColor={"#0070D2"}
-                            maximumTrackTintColor="#C4C4C4"                            thumbTintColor={colors.coreBlue}
+                            maximumTrackTintColor="#C4C4C4" thumbTintColor={colors.coreBlue}
                             onValueChange={onSliderValueChange3}
                         />
                     </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: horizontalScale(15), marginTop: verticalScale(-8) }}>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>
+                    <View style={styles.numberView}>
+                        <Text style={styles.number}>
                             1%
                         </Text>
-                        <Text style={{ color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 }}>
+                        <Text style={styles.number}>
                             25%
                         </Text>
                     </View>
 
                     <TextInput
+                        maxLength={2}
                         keyboardType="numeric"
-                        style={{ borderBottomWidth: 0.5, borderBottomColor: 'grey', backgroundColor: 'transparent', maxWidth: '90%', marginHorizontal: horizontalScale(18) }}
+                        style={styles.inputbox}
                         onChangeText={onTextInputChange3}
                         onEndEditing={(event) => onTextInputEndEditing3(event.nativeEvent.text)}
                         value={text3}
-
-
                     />
 
                 </View>
 
                 {emi > 0 && (
-          <View style={{ alignItems: 'center', marginTop: verticalScale(25),  }}>
-            <Text style={{ fontSize: 24, fontWeight:'600', color: '#000000' }}>EMI: ₹{emi}</Text>
-          </View>
-        )}
+                    <View style={styles.emiview}>
+                        <Text style={styles.emi}>EMI: ₹{emi}</Text>
+                    </View>
+                )}
 
-<View style={styles.buttonview}>
-                            <Button
-                                onPress={() => {
-                                    calculateEMI()
-                                }}
-                                type="primary"
-                                label="Calculate EMI"
-                                buttonContainer={styles.button}
-                            />
-                        </View>
-        
-
-        {/* Display EMI */}
-        
-
+                <View style={styles.buttonview}>
+                    <Button
+                        onPress={() => {
+                            calculateEMI()
+                        }}
+                        type="primary"
+                        label="Calculate EMI"
+                        buttonContainer={styles.button}
+                    />
+                </View>
             </ScrollView>
         </View>
     )
@@ -242,10 +263,23 @@ const EmiCalculator = () => {
 export default EmiCalculator
 
 const styles = StyleSheet.create({
-    button:{
+    button: {
         marginTop: verticalScale(30),
         marginBottom: verticalScale(10),
-        backgroundColor:'#0076C7',
+        backgroundColor: '#0076C7',
         marginHorizontal: horizontalScale(120)
-    }
+    },
+    sliderstyle: {
+        width: '50%', alignSelf: 'center', transform: [{ scaleX: verticalScale(2), }, { scaleY: verticalScale(2) }], borderRadius: 20, height: verticalScale(30)
+    },
+    emi: {
+        fontSize: 24, fontWeight: '600', color: '#000000'
+    },
+    number: { color: "#3E3E3C", fontSize: 16, fontWeight: '400', lineHeight: 18 },
+    numberView: { flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: horizontalScale(15), marginTop: verticalScale(-8) },
+    emiview: { alignItems: 'center', marginTop: verticalScale(25), },
+    inputbox: { borderBottomWidth: 0.5, borderBottomColor: 'grey', backgroundColor: 'transparent', maxWidth: '90%', marginHorizontal: horizontalScale(18), marginTop: verticalScale(-5), marginBottom: verticalScale(5) },
+    star: { marginLeft: horizontalScale(20), color: "#C23934", bottom: verticalScale(2) },
+    title: { color: "#3E3E3C", fontSize: 14, fontWeight: '400', lineHeight: 18 },
+
 })
