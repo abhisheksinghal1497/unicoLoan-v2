0c57b69c55b04d5b9b5ab385eae0c27d48f17d82
abdulazizpwc <abdul.aziz.tpr@pwc.com>
2024-07-01 11:02:35 +0530

    Redo changes

--- android/app/src/main/java/com/unico_loan/react/MainActivity.java
+++ android/app/src/main/java/com/unico_loan/react/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends SalesforceReactActivity {
 
 	@Override
 	public boolean shouldAuthenticate() {
-		return false;
+		return true;
 	}
 
 	/**
--- src/Navigation/MainNavigation.js
+++ src/Navigation/MainNavigation.js
@@ -37,7 +37,7 @@ const Dashboard = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}
-        initialRouteName={screens.CaptureSelfie}>
+        initialRouteName={screens.HomeScreen}>
         <Stack.Screen
           name={screens.Splash}
           component={Splash}
